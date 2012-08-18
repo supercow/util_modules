b1e2bda320c36c45558ba4f02a0afaba4de20bac
James Sweeny <james.sweeny@puppetlabs.com>
2012-08-18 13:15:03 -0400

    Add some extra misc packages

--- util_host/manifests/params.pp
+++ util_host/manifests/params.pp
@@ -3,6 +3,10 @@ class util_host::params {
   #minor packages that don't deserve their own module
   $misc_packages = [
     'nc',
+    'tree',
+    'tcpdump',
+    'vim-enhanced',
+    'nmap',
   ]
 
   #features (classes/modules) that will be included
