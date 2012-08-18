24d4dbd9d7f60e8d967e223067e3cf9294237dda
James Sweeny <james.sweeny@puppetlabs.com>
2012-08-17 22:08:46 -0400

    dnsmasq initial commit

---
+++ dnsmasq/manifests/init.pp
@@ -0,0 +1,24 @@
+class dnsmasq(
+  $puppet_dns_svr1 = '192.168.100.8',
+  $puppet_dns_svr2 = '192.168.100.7',
+) {
+
+  package { 'dnsmasq':
+    ensure => present,
+  }
+
+  file { '/etc/dnsmasq.conf':
+    ensure  => 'file',
+    content => template('dnsmasq/dnsmasq.conf.erb'),
+    group   => 'root',
+    mode    => '644',
+    owner   => 'root',
+    require => Package['dnsmasq'],
+  }
+
+  service { 'dnsmasq':
+    ensure    => 'running',
+    enable    => 'true',
+    subscribe => File['/etc/dnsmasq.conf'],
+  }
+}
---
+++ dnsmasq/manifests/params.pp
@@ -0,0 +1,4 @@
+class dnsmasq::params {
+  $puppet_dns_svr1 = 
+  $puppet_dns_svr2 = undef
+}
